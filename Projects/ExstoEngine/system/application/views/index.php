23b690b57652782f7bc054bd4298313abfe51f1c
CaptainNic <nicolasbreidinger@gmail.com>
2011-08-24 00:37:40 +0000

    Added real slideshow, still using example images until we have some of our own material.
    git-svn-id: https://subversion.assembla.com/svn/shadowrule/trunk@87 d986344e-3b64-40bb-a93d-205cfbe66891

--- Projects/ExstoEngine/system/application/views/index.php
+++ Projects/ExstoEngine/system/application/views/index.php
@@ -1,6 +1,33 @@
 <div style="display: table; margin-top: 20px;">
 	<div style="padding: 0px 0px 0px 5px; display: table-cell; width: 500px; vertical-align: top;">
-		<img src="<?=base_url()?>/system/application/views/content/images/slideshowPlaceholder.png"/>
+		<link href="<?=base_url().'system/application/views/style/slideshow.css'?>" rel="stylesheet" type="text/css" />
+		<script src="<?=base_url()?>/lib/jquery-1.4.4.min.js"></script> 
+		<script src="http://gsgd.co.uk/sandbox/jquery/easing/jquery.easing.1.3.js"></script> 
+		<script src="<?=base_url()?>/lib/slides.min.jquery.js"></script> 
+		<script> 
+			$(function(){
+				$('#slides').slides({
+					preload: true,
+					preloadImage: 'img/loading.gif',
+					play: 5000,
+					pause: 2500,
+					hoverPause: true
+				});
+			});
+		</script>
+		<div id="slides"> 
+			<div class="slides_container"> 
+				<a href="http://www.flickr.com/photos/jliba/4665625073/" title="145.365 - Happy Bokeh Thursday! | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-1.jpg" width="570" height="270" alt="Slide 1"></a> 
+				<a href="http://www.flickr.com/photos/stephangeyer/3020487807/" title="Taxi | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-2.jpg" width="570" height="270" alt="Slide 2"></a> 
+				<a href="http://www.flickr.com/photos/childofwar/2984345060/" title="Happy Bokeh raining Day | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-3.jpg" width="570" height="270" alt="Slide 3"></a> 
+				<a href="http://www.flickr.com/photos/b-tal/117037943/" title="We Eat Light | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-4.jpg" width="570" height="270" alt="Slide 4"></a> 
+				<a href="http://www.flickr.com/photos/bu7amd/3447416780/" title="“I must go down to the sea again, to the lonely sea and the sky; and all I ask is a tall ship and a star to steer her by.” | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-5.jpg" width="570" height="270" alt="Slide 5"></a> 
+				<a href="http://www.flickr.com/photos/streetpreacher/2078765853/" title="twelve.inch | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-6.jpg" width="570" height="270" alt="Slide 6"></a> 
+				<a href="http://www.flickr.com/photos/aftab/3152515428/" title="Save my love for loneliness | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-7.jpg" width="570" height="270" alt="Slide 7"></a> 
+			</div> 
+			<a href="#" class="prev"><img src="<?=base_url()?>/system/application/views/content/images/arrow-prev.png" width="24" height="43" alt="Arrow Prev"></a> 
+			<a href="#" class="next"><img src="<?=base_url()?>/system/application/views/content/images/arrow-next.png" width="24" height="43" alt="Arrow Next"></a> 
+		</div> 
 	</div>
 </div>
 
