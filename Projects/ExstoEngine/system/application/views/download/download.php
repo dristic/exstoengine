9922dda54df04c7984ca7442dc035d6779395506
CaptainNic <nicolasbreidinger@gmail.com>
2011-10-14 21:51:12 +0000

    Updated download page with exstoengine 0.6.0 download link
    git-svn-id: https://subversion.assembla.com/svn/shadowrule/trunk@159 d986344e-3b64-40bb-a93d-205cfbe66891

--- Projects/ExstoEngine/system/application/views/download/download.php
+++ Projects/ExstoEngine/system/application/views/download/download.php
@@ -1,5 +1,8 @@
 <h2>Download</h2>
 
-<p>The current version is: 0.3.0</p>
+<p>Current Version:</p>
 
-<a href="../../lib/exstoengine-0.3.0.zip">Download</a>
+<a href="../../lib/exstoengine-0.6.0.zip">Download ExstoEngine - 0.6.0</a>
+
+<p>Older Releases:</p>
+<a href="../../lib/exstoengine-0.3.0.zip">Download ExstoEngine - 0.3.0</a>
