492178eb3746a62bdfd8fd17ca661ee00105f241
CaptainNic <nicolasbreidinger@gmail.com>
2011-10-22 10:05:17 +0000

    New build: 0.6.2  - zipped with changelog  - download page updated
    git-svn-id: https://subversion.assembla.com/svn/shadowrule/trunk@190 d986344e-3b64-40bb-a93d-205cfbe66891

--- Projects/ExstoEngine/system/application/views/download/download.php
+++ Projects/ExstoEngine/system/application/views/download/download.php
@@ -2,10 +2,13 @@
 
 <h3>Current Version:</h3>
 <p>
-	<a href="../../lib/exstoengine/build/exstoengine-0.6.1.zip">ExstoEngine - 0.6.1</a>
+	<a href="../../lib/exstoengine/build/exstoengine-0.6.2.zip">ExstoEngine - 0.6.2</a>
 </p>
 
 <h3>Older Releases:</h3>
+<p>
+	<a href="../../lib/exstoengine/build/exstoengine-0.6.1.zip">ExstoEngine - 0.6.1</a>
+</p>
 <p>
 	<a href="../../lib/exstoengine/build/exstoengine-0.6.0.zip">ExstoEngine - 0.6.0</a>
 </p>
