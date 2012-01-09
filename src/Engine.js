1eb161a141853614c457199c7ed8eb5e2a46aa2a
Dan Ristic <danr@exstoengine.com>
2012-01-09 13:02:18 -0700

    Add dynamic frame rate for physics.

--- src/Engine.js
+++ src/Engine.js
@@ -31,6 +31,7 @@ ex.using([
 			
 			//--Public
 			this.frameRate = frameRate || 50;
+			this.deltaTime = 1 / this.frameRate;
 			this.width = width;
 			this.height = height;
 			this.fullscreen = false;
@@ -91,31 +92,16 @@ ex.using([
 			var newTime = (new Date()).getTime();
 			var dt = newTime - this.lastTime;
 			dt /= 1000;
+			var frameTime = dt;
 			this.lastTime = newTime;
 			
-			//--Step components
-			var i = this.components.length;
-			while(i--) {
-				this.components[i].update(dt);
+			while(frameTime > 0) {
+			  var deltaTime = Math.min(frameTime, this.deltaTime);
+			  this.integrate(this.deltaTime);
+			  frameTime -= this.deltaTime;
 			}
 			
-			//--Step world
-			if(this.currentWorld != null) {
-				this.currentWorld.update(dt);
-			}
-			
-			//--Step collision manager
-			if(this.collisionManager != null) {
-				this.collisionManager.update(dt);
-			}
-			
-			//--Step camera
-			this.camera.update(dt);
-			
-			//--Step renderer
-			if(this.renderer != null) {
-				this.renderer.update(dt, this.camera);
-			}
+			this.render(dt);
 			
 			this.onUpdate(dt);
 			
@@ -124,6 +110,34 @@ ex.using([
 			if(this.debug){
 				ex.Debug.benchmarkEngine(dt);
 			}	
+		},
+		
+		integrate: function (dt) {
+		  //--Step components
+      var i = this.components.length;
+      while(i--) {
+        this.components[i].update(dt);
+      }
+      
+      //--Step world
+      if(this.currentWorld != null) {
+        this.currentWorld.update(dt);
+      }
+      
+      //--Step collision manager
+      if(this.collisionManager != null) {
+        this.collisionManager.update(dt);
+      }
+		},
+		
+		render: function (dt) {
+		  //--Step camera
+      this.camera.update(dt);
+      
+      //--Step renderer
+      if(this.renderer != null) {
+        this.renderer.update(dt, this.camera);
+      }
 		},
 		
 		onUpdate: function() {
