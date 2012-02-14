c75804fe24873b9b70fa692fd75a1f097cc9bda0
Dan Ristic <danr@exstoengine.com>
2012-02-13 17:04:24 -0700

    Add cache features to device status.

--- src/util/Device.js
+++ src/util/Device.js
@@ -9,6 +9,53 @@ ex.using([
         return navigator.onLine;
       },
       
+      cache: {
+        UNCACHED: 0,
+        IDLE: 1,
+        CHECKING: 2,
+        DOWNLOADING: 3,
+        UPDATEREADY: 4,
+        OBSOLETE: 5,
+        
+        status: function () {
+          var appCache = window.applicationCache;
+          
+          switch(appCache.status) {
+            case appCache.UNCACHED:
+              return ex.Device.cache.UNCACHED;
+              break;
+            case appCache.IDLE:
+              return ex.Device.cache.IDLE;
+              break;
+            case appCache.CHECKING:
+              return ex.Device.cache.CHECKING;
+              break;
+            case appCache.DOWNLOADING:
+              return ex.Device.cache.DOWNLOADING;
+              break;
+            case appCache.UPDATEREADY:
+              return ex.Device.cache.UPDATEREADY;
+              break;
+            case appCache.OBSOLETE:
+              return ex.Device.cache.OBSOLETE;
+              break;
+          }
+        },
+        
+        update: function (callback) {
+          window.applicationCache.addEventListener('updateready', function(e) {
+            if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+              window.applicationCache.swapCache();
+              callback();
+            } else {
+              // Manifest didn't changed. Nothing new to server.
+            }
+          }, false);
+          
+          window.applicationCache.update();
+        }
+      }
+      
       supports: {
         canvas: function () {
           var el = document.createElement('canvas');
